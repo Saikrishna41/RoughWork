a4a18cb62d536c6d151036399973a3dedff7add1
Saikrishna Ramalingam <saai.krrishna@gmail.com>
2020-11-10 22:51:15 -0600

    viewmodel and data class

---
+++ app/src/main/java/com/tpinfo/roughwork/data/Monster.kt
@@ -0,0 +1,12 @@
+package com.tpinfo.roughwork.data
+
+data class Monster (
+
+    val monsterName : String,
+    val description : String,
+    val imageFile : String,
+    val caption : String,
+    val price : Double,
+    val scariness : Int
+
+)
---
+++ app/src/main/java/com/tpinfo/roughwork/viewmodel/SharedViewModel.kt
@@ -0,0 +1,9 @@
+package com.tpinfo.roughwork.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class SharedViewModel(app: Application) : AndroidViewModel(app){
+
+
+}
